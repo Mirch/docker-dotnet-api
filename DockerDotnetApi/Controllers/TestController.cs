d5ee987b6bdee426bb69a83f0ef26746c5ac9ddd
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2019-10-01 20:49:27 +0200

    Add test project

---
+++ DockerDotnetApi/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DockerDotnetApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "The API is working!" });
+        }
+    }
+}
